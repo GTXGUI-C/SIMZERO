0f9f27ff42ae2e6348193d60910743a45ea4f751
嘉杰 吴 <670269305@qq.com>
2022-09-14 17:09:44 +0800

    Initial commit

---
+++ src/main/java/com/example/simzero/DemandGenerator/Data.java
@@ -0,0 +1,13 @@
+package com.example.simzero.DemandGenerator;
+
+public class Data {
+    private String name; // 数据名称
+    private String industry; // 数据所属产业
+    private String owner; // 数据持有人
+    private Double holdingCost; // 持有成本
+    private Boolean isVerified; // 数据是否被认证（合规） True 认证 False 未认证
+
+    public void Setter() {
+        this.industry = industry;
+    }
+}
---
+++ src/test/java/com/example/simzero/SimzeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simzero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimzeroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
